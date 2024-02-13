6ebfee8b061a5638848e4f41a14e6a72ff859902
2dilution2 <2dilution2@gmail.com>
2024-02-13 16:49:15 +0900

    [feat #2] 엔티티 구현

--- src/main/java/com/thinktank/_66daysserver/domain/habit/model/Habit.java
+++ src/main/java/com/thinktank/_66daysserver/domain/habit/model/Habit.java
@@ -1,4 +1,34 @@
 package com.thinktank._66daysserver.domain.habit.model;
 
-public class Habit {
+import java.time.LocalDate;
+
+import com.thinktank._66daysserver.domain.users.model.Users;
+import com.thinktank._66daysserver.global.entity.BaseTime;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "habit")
+@Getter
+@Setter
+public class Habit extends BaseTime {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long habitID;
+
+	@ManyToOne
+	@JoinColumn(name = "userID", nullable = false)
+	private Users user;
+
+	private String habitName;
+
+	private String description;
+
+	private LocalDate startDate;
+
+	private LocalDate endDate;
+
 }
--- src/main/java/com/thinktank/_66daysserver/domain/users/model/Users.java
+++ src/main/java/com/thinktank/_66daysserver/domain/users/model/Users.java
@@ -1,4 +1,25 @@
 package com.thinktank._66daysserver.domain.users.model;
 
-public class Users {
+import com.thinktank._66daysserver.global.entity.BaseTime;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class Users extends BaseTime {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userID;
+
+	private String name;
+
+	private String email;
+
+	private String password;
+
 }
