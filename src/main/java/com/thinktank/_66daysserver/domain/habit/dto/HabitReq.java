f87e4aadf279ff7368673959c9ae6af29cba7acf
2dilution2 <2dilution2@gmail.com>
2024-02-15 17:01:10 +0900

    [fix #12] 습관 종료일자 서비스 수정

--- src/main/java/com/thinktank/_66daysserver/domain/habit/dto/HabitReq.java
+++ src/main/java/com/thinktank/_66daysserver/domain/habit/dto/HabitReq.java
@@ -1,6 +1,7 @@
 package com.thinktank._66daysserver.domain.habit.dto;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -11,7 +12,6 @@ public class HabitReq {
 
 	private String habitName;
 	private LocalDate startDate;
-	private LocalDate reminderTime;
-	// endDate는 startDate로부터 66일 후로 자동 계산되므로 포함하지 않음
+	private LocalTime reminderTime;
 
 }
